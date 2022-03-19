695ae477f6f3e2fcf9b141552a4aa2a56a664c4c
Dustin Firebaugh <dafirebaugh@gmail.com>
2022-03-19 12:37:12 -0400

    updating some dependencies

--- src/module-info.java
+++
@@ -1,7 +0,0 @@
-module scheduling.app {
-    requires javafx.fxml;
-    requires javafx.controls;
-
-    opens scheduling;
-    opens scenes;
-}
---
+++ src/scenes/SceneControllerTest.java
@@ -0,0 +1,14 @@
+package scenes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SceneControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void switchToHome() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setScene() {
+    }
+}
